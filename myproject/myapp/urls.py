e5df30b04f9555cbfdfdb9a5be40d47a9ba611f2
Joe W <125931177+JDoubleUwU@users.noreply.github.com>
2025-07-21 18:55:48 -0700

    Add files via upload

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+from . import views
+
+app_name = 'myapp'
+
+urlpatterns = [
+    path('test/', views.test, name='test'),
+
+    path('doctors/', views.DoctorsListView.as_view(), name='DoctorsListView'),
+    path('doctors/<int:pk>', views.DoctorsDetailView.as_view(), name='DoctorsDetail'),
+    path('doctors/create', views.DoctorsCreateView.as_view(), name='DoctorsCreate'),
+    path('doctors/<int:pk>/update', views.DoctorsUpdateView.as_view(), name='DoctorsUpdate'),
+    path('doctors/<int:pk>/delete', views.DoctorsDeleteView.as_view(), name='DoctorsDelete'),
+]
